90f915bb16edfaa1f93d7ee43af81d891c6a1bec
michelepiccinno <michele.piccinno@gmail.com>
2023-07-07 07:54:05 +0200

    add-comment

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,14 +1,16 @@
 //ABBIAMO DUE ARRAY CONTENENTI 6 NOMI E 6 COGNOMI----- DOBBIAMO OTTENERE UN ARRAY CON NOMI E COGNOMI CASUALI//
 
-
 //CREO UN ARRAY NOMI, ARRAY COGNOMI, E ARRAY VUOTA
 const arrayNomi = ['Mario', 'Paolo', 'Laura', 'Genoveffa', 'Esterina', 'Pasqualino'];
 const arrayCognomi = ['Rossi', 'Zidane', 'Torricelli', 'Di Livio', 'Lombardo', 'Baresi'];
+
 //INIZIALIZZO UN ARRAY VUOTO
 let arrayNomiCognomi = [];
 
 //SPIEGAZIONE DEL FOR: ( i e j mathrandom; i < lenghtStartArrayNomi [che decresce ad ogni ciclo]; i e j mathrandom; )
-//IL MATHRANDOM SARA' DA 0 A 5-4-3-2-1 (decresce dinamicamente perche' svuoto l'array con lo splice man mano che si popola l'array vuota)
+//MATHFLOOR TRONCA I DECIMALI PERTANTO AVREMO INDICE MAX 5
+//GLI INDICI ASSUMERANNO I VALORI 5-4-3-2-1 (decrescono dinamicamente perche' svuoto i 2 array iniziali con lo splice ad ogni ciclo e popolo l'array vuota)
+//PER I MOTIVI SOPRA RIPORTATI L'ALGORITMO NON DOVREBBE MAI CICLARE A VUOTO
 for (let i = Math.floor(Math.random() * (arrayNomi.length)), j = Math.floor(Math.random() * (arrayNomi.length));
     i < arrayNomi.length;
     i = Math.floor(Math.random() * (arrayNomi.length)), j = Math.floor(Math.random() * (arrayNomi.length))) {
